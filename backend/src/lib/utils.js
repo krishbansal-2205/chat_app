bc5ba63f9d26ddc9f56c4c7b58e1ed940ea0135c
Krish Bansal <krishbansalkssk@outlook.com>
2025-04-14 17:16:27 +0530

    fix: update cookie settings for JWT to enhance security and compatibility

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -8,8 +8,8 @@ export const generateToken = (userId, res) => {
    res.cookie('jwt', token, {
       httpOnly: true,
       maxAge: 7 * 24 * 60 * 60 * 1000,
-      sameSite: 'strict',
-      secure: process.env.NODE_ENV !== 'development',
+      secure: true,
+      sameSite: 'None',
    });
 
    return token;
